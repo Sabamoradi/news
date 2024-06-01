03881f05626bad97ca5a8cd7b51fe1402b5ab4dc
sabamoradi <saba.moradi7719@gmail.com>
2024-06-01 15:52:57 +0330

    basics of redux and add search container

--- src/Layout/Header/index.tsx
+++ src/Layout/Header/index.tsx
@@ -1,9 +1,23 @@
-const Header = () =>{
-  return(
-    <>
-      
-    </>
-  )
-}
+import { AudioOutlined } from "@ant-design/icons";
+import { Input, Space } from "antd";
+import type { SearchProps } from "antd/es/input/Search";
+import "./style.scss";
+
+const { Search } = Input;
+
+const Header = () => {
+  return (
+    <div className="header_container">
+      <div className="input_wrapper">
+        <Search
+          placeholder="input search text"
+          allowClear
+          enterButton="Search"
+          size="large"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- src/services/index.tsx
+++ src/services/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiFirstBaseURL = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1038e30893ee46d296c0e6e36a8bdbfc'; // newsapi
 const apiSecondBaseURL = 'https://content.guardianapis.com/search?page=1&api-key=f00667d7-6940-435f-8d7e-de4d135651ef';  //The Guardian
-const apiThird = 'https://bbc-api.vercel.app/latest?lang=english' //BBC
+const apiThird = 'https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=vwjNDWt6gDLhcFNaeuzhCUcQpdI1FAOP' //times
 
 export const httpService = axios.create({
   baseURL: apiFirstBaseURL,
